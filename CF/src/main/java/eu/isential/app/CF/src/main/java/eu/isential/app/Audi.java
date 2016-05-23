1e1f730c093af9cc2a5dbd28897ebbe62cdbae75
jholtkamp <josholtkamp@hotmail.com>
2016-05-23 14:32:39 +0200

    wijziging verwijderen package

--- CF/src/main/java/eu/isential/app/CF/src/main/java/eu/isential/app/Audi.java
+++
@@ -1,8 +0,0 @@
-package eu.isential.app;
-
-public class Audi implements Car {
-	public void drive(){
-		System.out.println("De Audi rijd de fabriek uit, FROOOMM");
-	}
-	
-}
--- CF/src/main/java/eu/isential/app/CF/src/main/java/eu/isential/app/VW.java
+++
@@ -1,9 +0,0 @@
-package eu.isential.app;
-
-public class VW implements Car {
-	
-	public void drive(){
-		System.out.println("De VW rijd de fabriek uit, FROOOMM");
-	}
-
-}
